d8a6dc87cc9063c5ec62031a169da1d08c02d48e
wangzheng <zheng.wang@fih-foxconn.com>
2021-01-19 11:11:19 +0800

    add multi-entry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import printMe from './print';
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
   element.innerHTML = 'Hello Webpack';
-
+  btn.innerHTML = 'Click Me';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
